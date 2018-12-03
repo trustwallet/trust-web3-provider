18ae3507f8d0186af5a824b7d96b9d33d69add9a
hewig <360470+hewigovens@users.noreply.github.com>
2018-12-03 14:35:25 +0800

    Update podspec (#44)
    * bump podspec tag
    
    * Fix ready check
    
    * match metamask response format

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import IdMapping from "./id_mapping";
 class TrustWeb3Provider {
   constructor(config) {
     this.address = config.address;
-    this.readonly = !!config.address;
+    this.ready = !!config.address;
     this.chainId = config.chainId;
     this.rpc = new RPCServer(config.rpcUrl);
     this.filterMgr = new FilterMgr(this.rpc);
@@ -25,13 +25,16 @@ class TrustWeb3Provider {
 
   setAddress(address) {
     this.address = address;
-    this.readonly = !!address;
+    this.ready = !!address;
   }
 
   enable() {
     return this._sendAsync({
       method: "eth_requestAccounts",
       params: []
+    })
+    .then(result => {
+      return result.result;
     });
   }
 
@@ -202,15 +205,16 @@ class TrustWeb3Provider {
   }
 
   postMessage(handler, id, data) {
-    if (this.readonly && handler !== "requestAccounts") {
-      // don't forget to verify in the app
-      this.sendError(id, new Error("provider is readonly"));
-    }
-    window.webkit.messageHandlers[handler].postMessage({
+    if (this.ready || handler === "requestAccounts") {
+      window.webkit.messageHandlers[handler].postMessage({
         "name": handler,
         "object": data,
         "id": id
       });
+    } else {
+      // don't forget to verify in the app
+      this.sendError(id, new Error("provider is not ready"));
+    }
   }
 
   sendResponse(id, result) {
