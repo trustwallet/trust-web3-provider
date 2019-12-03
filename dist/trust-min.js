8752b02c19a6261e67500281106ed5cb35770458
hewigovens <360470+hewigovens@users.noreply.github.com>
2019-12-03 15:28:48 +0800

    Add TrustWalletCore

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c83e2de48f0ff6b43bad2d37f5e86fa515b338dcb012089bfed01ab3a44bbc3c
-size 856556
+oid sha256:fd5e12f005d32f234f2e1736452109d41d8b93cccfbfce5aeb9a022f08641613
+size 856582
