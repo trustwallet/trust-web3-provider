3e5882bd693b1e362094ac13f1387fb595c84613
hewigovens <360470+hewigovens@users.noreply.github.com>
2020-09-16 09:03:30 +0800

    Add build:android

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c741bc4a7c53b7437f568d05f7a51873a62f86c0b7811c0fcd6f149b06a9503e
-size 533442
+oid sha256:57da87827804dd0e9a15be172266d949af959cafd5b628f04f12d1919ca70157
+size 533403
