31f336ffb7f30153afe6f393e2cf5c616b4a1d85
hewigovens <360470+hewigovens@users.noreply.github.com>
2020-12-21 18:30:34 +0900

    Fix bscscan write contract

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fa92304a0135af876702ecfe6a9582ff78c65f504289fc9bccd866ceba51017
-size 535161
+oid sha256:036a32854868cbd331c3d5bb65dad5c2c3b683ffd812ad7162d16b2feae6cd44
+size 535231
