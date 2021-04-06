0d080b4d19a4013da69a8924f2760818ac82ad46
hewigovens <360470+hewigovens@users.noreply.github.com>
2021-04-06 22:54:34 +0800

    set address for iframes

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f125c6e2ea85c391af8834fbdc74817bec2275256b2208fbb05e9ce2a43409d9
-size 535775
+oid sha256:d8963e1bff7e144011a16a988f79eaa7a94a96ece6f2c1946172a9e7d0d608ad
+size 535911
