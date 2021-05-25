93342ed7e681905d325477be3b03ff222a93f512
hewigovens <360470+hewigovens@users.noreply.github.com>
2021-05-25 08:40:41 +0800

    update package-lock.json

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d25350d73a927a105e85cc62599eb23dcdc85d76baeba49dbab1925002fafdb
-size 753527
+oid sha256:f7368e6dafdac21c70b2eb25366de723872e92209d9e37c6c54c879f872511f8
+size 780605
