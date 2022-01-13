48945ac647071e391a87a1f940a3aa07871046cf
s749312025 <749312025@qq.com>
2022-01-13 17:26:52 +0800

    rebuild dis/trust-min.js

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6237a28f928210863ab9cff0f7f17db255c51227a6a44aedb6fd167181fd8870
-size 780735
+oid sha256:b4422a4e91fa1da6f0b361f9db4dece749fad95d7a80266abc8d78463feb3f1e
+size 781849
