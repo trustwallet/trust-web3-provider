9862777101badcfa4fcec705bf89a2ca942825a2
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-02-15 05:01:26 +0900

    implement switch chain (#218)

--- ios/TrustWeb3Provider/WKWebView+Extensions.swift
+++ ios/TrustWeb3Provider/WKWebView+Extensions.swift
@@ -8,21 +8,52 @@ import Foundation
 import WebKit
 
 extension WKWebView {
+
+    public func setAddress(_ address: String) {
+        evaluateJavaScript("ethereum.setAddress(\"\(address)\");")
+    }
+
+    func setConfig(_ config: WKUserScriptConfig) {
+        let script = """
+        var config = {
+            address: "\(config.address)",
+            chainId: \(config.chainId),
+            rpcUrl: "\(config.rpcUrl)"
+        };
+        ethereum.setConfig(config);
+        """
+        print(script)
+        evaluateJavaScript(script)
+    }
+
+    func emitChange(chainId: Int) {
+        let string = "0x" + String(chainId, radix: 16)
+        let script = "ethereum.emitChainChanged(\"\(string)\");"
+        print(script)
+        evaluateJavaScript(script)
+    }
+
     public func sendError(_ error: String, to id: Int64) {
-        let script = String(format: "window.ethereum.sendError(%ld, \"%@\")", id, error)
+        let script = String(format: "ethereum.sendError(%ld, \"%@\")", id, error)
         print(script)
         evaluateJavaScript(script)
     }
 
     public func sendResult(_ result: String, to id: Int64) {
-        let script = String(format: "window.ethereum.sendResponse(%ld, \"%@\")", id, result)
+        let script = String(format: "ethereum.sendResponse(%ld, \"%@\")", id, result)
+        print(script)
+        evaluateJavaScript(script)
+    }
+
+    public func sendNull(to id: Int64) {
+        let script = String(format: "ethereum.sendResponse(%ld, null)", id)
         print(script)
         evaluateJavaScript(script)
     }
 
     public func sendResults(_ results: [String], to id: Int64) {
         let array = results.map { String(format: "\"%@\"", $0) }
-        let script = String(format: "window.ethereum.sendResponse(%ld, [%@])", id, array.joined(separator: ","))
+        let script = String(format: "ethereum.sendResponse(%ld, [%@])", id, array.joined(separator: ","))
         print(script)
         evaluateJavaScript(script)
     }
