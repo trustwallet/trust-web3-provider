9399e9c6c6027e050b842388a633d0469543ce8d
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-05-07 10:53:27 +0900

    Check result is null (#272)
    * check result is null
    
    * disable debug

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf0942907f0813c1eda0e97569f71c458bfd3598368d4c889858dee6d4d3f6ce
-size 297
+oid sha256:632614d0e23d9511fd06d3a1356c4a8a8ab55649208dc98ef217c924a832380c
+size 283
