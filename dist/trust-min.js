7599f74a8d07e627c1699ead232eeb95b6e1cd2e
Ruslan Serebriakov <rsrbk1@gmail.com>
2022-07-06 13:42:35 +0400

    update files

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f80fe4110f9acf6f0d7e8546e1f17a5186657ed354565b505b256a023d3274b
-size 951064
+oid sha256:fbcc1f9f0b556455dd322549f51aaedf1106930539296ac4dc67f037ea704fcc
+size 951060
