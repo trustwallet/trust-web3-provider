2a61f6e00f6cdeb1534fa2c330f2b6e573ff3df5
Ruslan Serebriakov <rsrbk1@gmail.com>
2022-07-06 10:27:39 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ class TrustWeb3Provider extends EventEmitter {
     // this points to window in methods like web3.eth.getAccounts()
     var that = this;
     if (!(this instanceof TrustWeb3Provider)) {
-      that = window.ethereum;
+      that = window.solana;
     }
     return that._request(payload, false);
   }
