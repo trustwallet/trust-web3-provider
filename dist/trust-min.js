8f3ca5b95796f97f58b37d653e28c2c2a7b496af
Ruslan Serebriakov <rsrbk1@gmail.com>
2022-07-07 14:41:01 +0400

    update payload id

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d38d41fb414db2c34c47b9b6fc1517561e6f2cea2af09348376bc6997c297fe
-size 1270972
+oid sha256:31117ad55669a2cd25f60573a8f09879238c10fd5e63cde0beaa77d0065fc549
+size 1270963
