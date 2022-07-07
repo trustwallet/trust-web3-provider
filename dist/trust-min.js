1ac3dbca39ce1924189c20e77bcee1448c655760
Ruslan Serebriakov <rsrbk1@gmail.com>
2022-07-07 14:27:13 +0400

    signTransaction payload as an array

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d38d41fb414db2c34c47b9b6fc1517561e6f2cea2af09348376bc6997c297fe
-size 1270972
+oid sha256:e034fe62eb347c58463589095e37177e9abfd2e7cd35c34e72d957fba366ccc3
+size 1270974
