a8cbc78e87cfb2d99cf4ff1375ddf0c0440adc77
hewigovens <360470+hewigovens@users.noreply.github.com>
2022-09-21 09:22:32 +0900

    build min.js

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db8f479d502924ebd660e9bdaa1cc431980dec122d66f403f25dd2e0ff54f38e
-size 895529
+oid sha256:4389eb388e28ca348d9a1ab0bfd73d65491436b310a84902281db491173b1525
+size 895536
