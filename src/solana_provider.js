97c3edaf06364a88a1ce386cf44e89353fb7e154
Yeferson Licet <yeferson.licet@binance.com>
2023-03-01 00:21:55 -0300

    feat: New method to allow multiple transactions

--- src/solana_provider.js
+++ src/solana_provider.js
@@ -61,6 +61,23 @@ class TrustSolanaWeb3Provider extends BaseProvider {
     });
   }
 
+  mapSignedTransaction(tx, signatureEncoded) {
+    const version = typeof tx.version !== "number" ? "legacy" : tx.version;
+    const signature = bs58.decode(signatureEncoded);
+
+    tx.addSignature(this.publicKey, signature);
+
+    if (version === "legacy" && !tx.verifySignatures()) {
+      throw new ProviderRpcError(4300, "Invalid signature");
+    }
+
+    if (this.isDebug) {
+      console.log(`==> signed single ${JSON.stringify(tx)}`);
+    }
+
+    return tx;
+  }
+
   signTransaction(tx) {
     const data = JSON.stringify(tx);
     const version = typeof tx.version !== "number" ? "legacy" : tx.version;
@@ -70,20 +87,9 @@ class TrustSolanaWeb3Provider extends BaseProvider {
     );
 
     return this._request("signRawTransaction", { data, raw, version })
-      .then((signatureEncoded) => {
-        const signature = bs58.decode(signatureEncoded);
-        tx.addSignature(this.publicKey, signature);
-
-        if (version === "legacy" && !tx.verifySignatures()) {
-          throw new ProviderRpcError(4300, "Invalid signature");
-        }
-
-        if (this.isDebug) {
-          console.log(`==> signed single ${JSON.stringify(tx)}`);
-        }
-
-        return tx;
-      })
+      .then((signatureEncoded) =>
+        this.mapSignedTransaction(tx, signatureEncoded)
+      )
       .catch((error) => {
         console.log(`<== Error: ${error}`);
       });
@@ -95,28 +101,22 @@ class TrustSolanaWeb3Provider extends BaseProvider {
 
   signAllTransactionsV2(txs) {
     return this._request("signRawTransactionMulti", {
-      transactions: txs.map((tx) => ({
-        data: JSON.stringify(tx),
-        raw: bs58.encode(tx.serializeMessage()),
-      })),
+      transactions: txs.map((tx) => {
+        const data = JSON.stringify(tx);
+        const version = typeof tx.version !== "number" ? "legacy" : tx.version;
+
+        const raw = bs58.encode(
+          version === "legacy" ? tx.serializeMessage() : tx.serialize()
+        );
+
+        return { data, raw, version };
+      }),
     })
-      .then((signatureEncoded) => {
-        return signatureEncoded.map((tx) => {
-          const signature = bs58.decode(signatureEncoded);
-
-          tx.addSignature(this.publicKey, signature);
-
-          if (!tx.verifySignatures()) {
-            throw new ProviderRpcError(4300, "Invalid signature");
-          }
-
-          if (this.isDebug) {
-            console.log(`==> signed single ${JSON.stringify(tx)}`);
-          }
-
-          return tx;
-        });
-      })
+      .then((signaturesEncoded) =>
+        signaturesEncoded.map((signature, i) =>
+          this.mapSignedTransaction(txs[i], signature)
+        )
+      )
       .catch((error) => {
         console.log(`<== Error: ${error}`);
       });
@@ -163,8 +163,9 @@ class TrustSolanaWeb3Provider extends BaseProvider {
         case "signMessage":
           return this.postMessage("signMessage", id, payload);
         case "signRawTransaction":
-        case "signRawTransactionMulti":
           return this.postMessage("signRawTransaction", id, payload);
+        case "signRawTransactionMulti":
+          return this.postMessage("signRawTransactionMulti", id, payload);
         case "requestAccounts":
           return this.postMessage("requestAccounts", id, {});
         default:
