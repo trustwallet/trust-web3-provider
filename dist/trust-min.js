bc3a4d24ecd446c25148443b900ff8c9755252f3
Yeferson Licet <yeferson.licet@binance.com>
2023-03-22 12:55:04 -0300

    fix lowercase address

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a53075ea4162e44b48ffb7803fea022b7a7778f6871a65cb7e7199b07d15bc2
-size 905614
+oid sha256:837c8cbf02bfff55633548909e282752d0d9ec99c3c1ef64d0c0a57119e8224d
+size 905642
