08a0b5f386dbf1a8434735d998b3d65aad0d84ab
Yeferson Licet <yeferson.licet@binance.com>
2023-10-23 13:30:17 -0300

    chore: Adding latest trust-min

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b909342aace2dbceb84932b805ba7512fca66af4948aec15353e0483986121b3
-size 905929
+oid sha256:d081a2a1a9f88442d8c0d7b2b6b3974017ca06e35d1bef89e7c9edc9ffc1f18b
+size 906087
