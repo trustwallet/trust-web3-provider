7034fdc8bcb124c3b83bd29af612068a1ec0e4c4
Yeferson Licet <111311418+y3fers0n@users.noreply.github.com>
2024-02-07 15:06:41 -0300

    chore: Refactoring RPC response parsing for cosmos (#500)

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7cfcf1c7453298f3998ec5bdd77a4ace4a029b71027135a29b8004cc36fb694
-size 906477
+oid sha256:df8966810f0970b613ddc5fc3440fc1d6bfca9c7538df35b9b41d0e6873d83a1
+size 906614
