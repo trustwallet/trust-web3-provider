2bfd36ade2ac2275782969dcae5ac6bf3d608054
Yeferson Licet <111311418+y3fers0n@users.noreply.github.com>
2024-02-21 12:27:19 -0300

    Adding solana standard support (#504)

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df8966810f0970b613ddc5fc3440fc1d6bfca9c7538df35b9b41d0e6873d83a1
-size 906614
+oid sha256:b3aaca1122853debbad2467dc6486a2839280d6ee22d03f4e5fa906f9ac38235
+size 1976890
