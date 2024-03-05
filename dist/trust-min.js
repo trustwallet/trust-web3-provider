137b224ad5da01bc427876eb3067244e14323343
Yeferson Licet <yeferson.licet@binance.com>
2024-03-05 11:09:14 -0300

    Fixing lowercase address verification

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df8966810f0970b613ddc5fc3440fc1d6bfca9c7538df35b9b41d0e6873d83a1
-size 906614
+oid sha256:1bbf2c6fa0739edc0724c9ee0a5e0e5fb0ac81946d4a83ee274586eb8040b81b
+size 906642
--- src/ethereum_provider.js
+++ src/ethereum_provider.js
@@ -265,7 +265,7 @@ class TrustWeb3Provider extends BaseProvider {
 
     if (
       typeof payload.params?.[0].toLowerCase() === "string" &&
-      this.address === payload.params?.[0].toLowerCase()
+      this.address.toLowerCase() === payload.params?.[0].toLowerCase()
     ) {
       message = payload.params[1];
       address = payload.params[0];
@@ -302,7 +302,7 @@ class TrustWeb3Provider extends BaseProvider {
 
     if (
       typeof payload.params?.[0] === "string" &&
-      this.address === payload.params?.[0].toLowerCase()
+      this.address.toLowerCase() === payload.params?.[0].toLowerCase()
     ) {
       data = payload.params[1];
       address = payload.params[0];
