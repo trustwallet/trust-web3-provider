47049ba77ef8fa25f6a2b45986b4b03b82b0853a
Yeferson Licet <yeferson.licet@trustwallet.com>
2024-10-04 18:02:13 -0300

    Build new nonce support

--- packages/ios-web3-provider/swift/trust-min.js
+++ packages/ios-web3-provider/swift/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c33a5fb09ea593ed3a5b5126671447dce7a21952274fdc83f285cdbd732870ae
-size 678407
+oid sha256:6c79dcf0c35185e2eb0c76af76c664661e291abf0b2c85285af15f01ec32ed10
+size 678453
