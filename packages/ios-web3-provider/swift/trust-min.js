b2be550f2ce4b6c8d4f3ed44bf2f44c121ec3807
Yeferson Licet <yeferson.licet@trustwallet.com>
2024-10-07 11:42:24 -0300

    Prevent object wrap

--- packages/ios-web3-provider/swift/trust-min.js
+++ packages/ios-web3-provider/swift/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52c54021c483b1472313ceb680765b858a4fb0e60d5a028e3d036263dcafd8a0
-size 678460
+oid sha256:ba7a609f4d7541fdf63691d95a01d8d70c08d61a0f13363a000ebeaf4c2c9a09
+size 678505
