db4138441f6837ecd12fd07cf7a7e9d7406afd1f
Yeferson Licet <yeferson.licet@trustwallet.com>
2024-10-14 09:29:38 -0300

    Adding new target version

--- packages/android-web3-provider/rollup.config.js
+++ packages/android-web3-provider/rollup.config.js
@@ -1,27 +1,27 @@
-import esbuild from 'rollup-plugin-esbuild';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { name, dependencies } from './package.json';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import inject from '@rollup/plugin-inject';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
 const input = './index.ts';
 const plugins = [
-  nodeResolve({ preferBuiltins: false, browser: true }),
+  nodePolyfills(),
+  resolve({ browser: true, preferBuiltins: false }),
   commonjs(),
+  babel({
+    babelHelpers: 'bundled',
+    extensions: ['.js', '.ts'],
+    exclude: 'node_modules/**',
+  }),
   inject({
     modules: {
       Buffer: ['buffer', 'Buffer'],
     },
   }),
-  nodePolyfills(),
-  esbuild({
-    minify: true,
-    tsconfig: './tsconfig.json',
-    loaders: {
-      '.json': 'json',
-    },
-  }),
+  terser(),
 ];
 
 function createConfig(
@@ -38,7 +38,6 @@ function createConfig(
       output: {
         file: '../../android/lib/src/main/res/raw/trust_min.js',
         format: 'umd',
-        exports: 'named',
         name: packageName,
         sourcemap: false,
         ...umd,
