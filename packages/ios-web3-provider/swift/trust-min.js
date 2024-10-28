6909f41cac772b0bf42607ab011c8a9c5d91d8d5
Yeferson Licet <yeferson.licet@trustwallet.com>
2024-10-28 13:25:07 -0300

    Rebase with main

--- packages/ios-web3-provider/swift/trust-min.js
+++ packages/ios-web3-provider/swift/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c78063f16667a5e03fb244b5a39e2c7bcfdf01a168e8f5f0b0dced938d9e7b57
-size 678494
+oid sha256:008f68e1a1c6822a418db5fe9a207a08463dcc6db3fbaa40114d8cd9b5554881
+size 678615
