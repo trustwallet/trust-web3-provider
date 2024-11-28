4f8ac36ec86852ac40d06813ae7c20662cc26f53
Yeferson Licet <yeferson.licet@trustwallet.com>
2024-11-28 14:54:02 -0300

    fix: Target chrome 67

--- packages/ios-web3-provider/swift/trust-min.js
+++ packages/ios-web3-provider/swift/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50c12fb92feb3d5d901cf1f69a1dc0995355525e5febf6f610572665ef279704
-size 684568
+oid sha256:5db373ecd452dcc54638df9768dfc737145c26e83a952a140aaf3236b11eed2c
+size 791008
