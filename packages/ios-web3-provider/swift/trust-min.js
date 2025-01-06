33a9acb853ff2040a924e74389374a7ec110fc7f
Yeferson Licet <yeferson.licet@trustwallet.com>
2025-01-06 15:25:40 -0300

    adding builds

--- packages/ios-web3-provider/swift/trust-min.js
+++ packages/ios-web3-provider/swift/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:667478b9f944de444df0b4412f5f152737e267d5188358c25d5aa506cfbbe4f1
-size 1123644
+oid sha256:f5ec8d0e0c9e9d14d967ef2c909a75672dcfd1f9adf2a2ccbec2fb3a97449f80
+size 1123966
