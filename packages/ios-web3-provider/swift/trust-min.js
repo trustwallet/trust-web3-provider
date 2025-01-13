622669a4e1b0423f5d1ae69111cb301835d78b26
Yeferson Licet <yeferson.licet@trustwallet.com>
2025-01-13 10:40:22 -0300

    fix: Symbol resolve for android

--- packages/ios-web3-provider/swift/trust-min.js
+++ packages/ios-web3-provider/swift/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23bbfda06906d6d3b9537ce5116b032c9a0ceddd880975e408272b9fe9ff606c
-size 683983
+oid sha256:007ebe97eb60c16a7a3d5f762414f10121ce7264e3f9960c403d99d924b61fcd
+size 675577
