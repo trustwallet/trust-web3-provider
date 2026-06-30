297743f7cd1d59136153f7744d3f9d08d89e68b4
arjunkumar811 <kumararjun26401@gmail.com>
2026-06-30 18:59:51 +0530

    fix(ton): upgrade @ton/ton to 16.2.4 to resolve circular dependency breaking bundle execution

--- packages/ios-web3-provider/swift/trust-min.js
+++ packages/ios-web3-provider/swift/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60bd2240db27987e6db552fef24c477b4b6c25628d4dc624420d134508351f37
-size 1124632
+oid sha256:ef40d64ce61acb6b7e0dfb088d7f8cd699514b9c0fa47a7486a1ae987e14d474
+size 1146631
